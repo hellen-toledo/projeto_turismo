387a9f9e6d7ee83986c6109d088815fb93608bed
hellen cristina toledo <hellenctoledos1516@gmail.com>
2026-02-10 21:56:45 -0300

    Criação da estrutura com Migrations e Models

---
+++ database/migrations/2026_02_11_003454_create_events_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); 
+            $table->string('day'); 
+            $table->string('month');
+            $table->string('location_city'); 
+            $table->string('action_link')->nullable(); 
+            $table->timestamps();
+});
+    }
+
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
